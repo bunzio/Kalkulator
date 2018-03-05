27e1fc875efe7018009743804bcb1dc1ada6f960
DESKTOP-P5A2NA1\bunze <bunzeladam@hotmail.com>
2018-03-05 22:07:14 +0100

    :)

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
-
+import javax.swing.JButton;
+import java.awt.FlowLayout;
 
 public class MyFrame extends JFrame {
 
@@ -9,7 +10,19 @@ public class MyFrame extends JFrame {
         setVisible(true);
         setSize(500, 780);
         setLocation(50, 50);
-        setResizable(false);
+        setLayout(new FlowLayout());
+
+        add(new JButton("1"));
+        add(new JButton("2"));
+        add(new JButton("3"));
+        add(new JButton("4"));
+        add(new JButton("5"));
+        add(new JButton("6"));
+        add(new JButton("7"));
+        add(new JButton("8"));
+        add(new JButton("9"));
+        setVisible(true);
+
     }
 }
 
