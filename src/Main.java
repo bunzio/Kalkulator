5e4960369fd3ed0d9e6c77abcf569e1d3406d5df
DESKTOP-P5A2NA1\bunze <bunzeladam@hotmail.com>
2018-03-05 21:47:22 +0100

    Okno programu

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-import java.util.Scanner;
+import java.awt.EventQueue;
 
 public class Main {
-   private static void dodawanie(String[] args){
-        double a, b;
-
-   }
     public static void main(String[] args) {
-        System.out.println("/////////////////////////////////");
-        System.out.println("//Witaj w prostym kalkulatorze//");
-        System.out.println("///////////////////////////////");
-        double a, b;
-        a = 1;
-        b = 2;
-        double wynik= 3; //dodawanie(a, b);
-        System.out.println(a + "+" + b + "=" + wynik);
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MyFrame();
 
+            }
+        });
     }
 }
---
+++ src/MyFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        super("Kalkulator by bunzio v1.0.0");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(500, 780);
+        setLocation(50, 50);
+        setResizable(false);
+    }
+}
+
+
