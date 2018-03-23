a747d689df564e335d75585cc25477c7671fe058
DESKTOP-P5A2NA1\bunze <bunzeladam@hotmail.com>
2018-03-23 15:57:02 +0100

    naprawa przecinka

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,20 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
-
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame  {
     private JButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, bDodwanie, bOdejmowanie,
                     bMnozenie, bDzielenie, bPrzecinek, bPlusMinus, bWynik, bKasuj, bCofnij, bOnawias, bZnawias, bPierwiastek,
                     bXdoN, bDo;
 
     private JTextField oknoWynik;
     private Container kontener = this.getContentPane();
-    private String licz, iText, oText;
+    private String licz, oText;
     private int przecinek = 0;
 
      MyFrame() {
-        setTitle("Kalkulator v1.0.0");
+        setTitle("Kalkulator v1.1.0");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize( 300, 250);
@@ -30,8 +28,9 @@ public class MyFrame extends JFrame {
         setBounds();
         setOknoWynik();
         ActionListener();
-
      }
+
+
     public  void initComponents() {
         //Utworzenie obiektow
         oknoWynik = new JTextField("0");
@@ -78,7 +77,6 @@ public class MyFrame extends JFrame {
             kontener.add(bMnozenie);
             kontener.add(bDzielenie);
             kontener.add(bPrzecinek);
-            kontener.add(bPlusMinus);
             kontener.add(bWynik);
             kontener.add(bKasuj);
             kontener.add(bCofnij);
@@ -87,6 +85,7 @@ public class MyFrame extends JFrame {
             kontener.add(bXdoN);
             kontener.add(bPierwiastek);
             kontener.add(bDo);
+            kontener.add(bPlusMinus);
         }
 
     public void setBounds(){
@@ -129,6 +128,7 @@ public class MyFrame extends JFrame {
         bDo.setBackground(Color.lightGray);
 
     }
+
     public void setOknoWynik(){
         oknoWynik.setHorizontalAlignment(JTextField.RIGHT);
         oknoWynik.setFont(new Font("System", Font.BOLD,20));
@@ -139,340 +139,191 @@ public class MyFrame extends JFrame {
         przecinek = 0;
     }
 
-    public void ActionListener() {
-        button0.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "0";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("0");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "1";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("1");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "2";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("2");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "3";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("3");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "4";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("4");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "5";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("5");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "6";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("6");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button7.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "7";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("7");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button8.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "8";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("8");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        button9.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = "9";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("9");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
-            }
-        });
-        bKasuj.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+    private void buttonAction(JButton buttonName){
+         oText = oknoWynik.getText();
+        if(oText.equals("0")) {
+            oknoWynik.setText(buttonName.getText());
+        }
+        else
+            oknoWynik.setText(oknoWynik.getText() + buttonName.getText());
+    }
+
+    private void buttonDzialanieAction(JButton buttonName){
+        oText = oknoWynik.getText();
+        if(oText.endsWith("+") || oText.endsWith("-") || oText.endsWith("*") || oText.endsWith("/") || oText.endsWith("(") ||
+                oText.endsWith(")") ) {
+            oText = oknoWynik.getText();
+        }
+        else{
+            if(oText.equals("0")){
                 oknoWynik.setText("0");
-                licz = "0";
-                oText = "0";
-                iText = "0";
+                przecinek = 0;
+            }
+            else{
+                oknoWynik.setText(oknoWynik.getText() + buttonName.getText());
+                przecinek = 0;
+            }
+        }
+
+
+
+    }
+    
+    public void ActionListener() {
+        button0.addActionListener(evt -> buttonAction(button0));
+        button1.addActionListener(evt -> buttonAction(button1));
+        button2.addActionListener(evt -> buttonAction(button2));
+        button3.addActionListener(evt -> buttonAction(button3));
+        button4.addActionListener(evt -> buttonAction(button4));
+        button5.addActionListener(evt -> buttonAction(button5));
+        button6.addActionListener(evt -> buttonAction(button6));
+        button7.addActionListener(evt -> buttonAction(button7));
+        button8.addActionListener(evt -> buttonAction(button8));
+        button9.addActionListener(evt -> buttonAction(button9));
+        bKasuj.addActionListener(evt -> {
+            oknoWynik.setText("0");
+            licz = "0";
+            oText = "0";
+            przecinek = 0;
+        });
+        bCofnij.addActionListener(evt -> {
+            int cofnij = oknoWynik.getText().length() - 1;
+
+            if(cofnij == 0) {
+                oknoWynik.setText("0");
+            }
+
+            if(cofnij > 0)
+            {
+                StringBuilder back = new StringBuilder(oknoWynik.getText());
+                back.deleteCharAt(cofnij);
+                oknoWynik.setText(back.toString());
+            }
+
+        });
+        bDodwanie.addActionListener(evt -> buttonDzialanieAction(bDodwanie));
+        bOdejmowanie.addActionListener(evt -> {
+            if(oText.equals("0")){
+                oknoWynik.setText("-");
+                przecinek = 0;
+            }
+            else{
+                oknoWynik.setText(oknoWynik.getText() + bOdejmowanie.getText());
                 przecinek = 0;
             }
         });
-        bCofnij.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                int cofnij = oknoWynik.getText().length() - 1;
-                String nowy;
-
-                if(cofnij == 0) {
-                    oknoWynik.setText("0");
-                }
-
-                if(cofnij > 0)
-                {
-                    StringBuilder back = new StringBuilder(oknoWynik.getText());
-                    back.deleteCharAt(cofnij);
-                    nowy = back.toString();
-                    oknoWynik.setText(nowy);
-                }
-
+        bMnozenie.addActionListener(evt -> buttonDzialanieAction(bMnozenie));
+        bDzielenie.addActionListener(evt -> buttonDzialanieAction(bDzielenie));
+        bXdoN.addActionListener(evt -> {
+            oText = oknoWynik.getText();
+            if(oText.equals("0")){
+                oknoWynik.setText("poteguj(");
             }
+            else{
+                licz = oText+"poteguj(";
+                oknoWynik.setText(licz);
+            }
+            przecinek = 0;
+
+
         });
-        bDodwanie.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        bOnawias.addActionListener(evt -> {
+            oText = oknoWynik.getText();
+            if(oText.equals("0")){
+                oknoWynik.setText("(");
+                licz = oknoWynik.getText();
+            }
+            else{
+                licz = oText+"(";
+                oknoWynik.setText(licz);
+            }
+            przecinek = 0;
+
+
+        });
+        bZnawias.addActionListener(evt -> buttonDzialanieAction(bZnawias));
+        bPierwiastek.addActionListener(evt -> {
+            oText = oknoWynik.getText();
+            if(oText.endsWith("+") || oText.endsWith("-") || oText.endsWith("*") || oText.endsWith("/") || oText.endsWith("(") ||
+                    oText.endsWith(")") ) {
                 oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("0");
-                }
-                else{
-                    licz = oText+"+";
-                    oknoWynik.setText(licz);
-                }
+            }
+            else{
+            if(oText.equals("0")){
+                oknoWynik.setText("√(");
+                licz = oknoWynik.getText();
                 przecinek = 0;
-
-
             }
-        });
-        bOdejmowanie.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("0");
-                }
-                else{
-                    licz = oText+"-";
-                    oknoWynik.setText(licz);
-                }
+            else{
+                licz = oText+"√(";
+                oknoWynik.setText(licz);
                 przecinek = 0;
+            }}
 
 
-            }
+
         });
-        bMnozenie.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        bPrzecinek.addActionListener(evt -> {
+            oText = oknoWynik.getText();
+            if(oText.endsWith("+") || oText.endsWith("-") || oText.endsWith("*") || oText.endsWith("/") || oText.endsWith("(") ||
+                    oText.endsWith(")") ){
                 oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("0");
-
-                }
-                else{
-                    licz = oText+"*";
-                    oknoWynik.setText(licz);
-                }
-                przecinek = 0;
-
-
             }
-        });
-        bDzielenie.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("0");
-                }
-                else{
-                    licz = oText+"÷";
-                    oknoWynik.setText(licz);
-                }
-                przecinek = 0;
-
-
-            }
-        });
-        bXdoN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("poteguj(");
-                }
-                else{
-                    licz = oText+"poteguj(";
-                    oknoWynik.setText(licz);
-                }
-                przecinek = 0;
-
-
-            }
-        });
-        bOnawias.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("(");
-                    licz = oknoWynik.getText();
-                }
-                else{
-                    licz = oText+"(";
-                    oknoWynik.setText(licz);
-                }
-                przecinek = 0;
-
-
-            }
-        });
-        bZnawias.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("0");
-                }
-                else{
-                    licz = oText+")";
-                    oknoWynik.setText(licz);
-                }
-                przecinek = 0;
-
-
-
-            }
-        });
-        bPierwiastek.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                if(oText.equals("0")){
-                    oknoWynik.setText("√(");
-                    licz = oknoWynik.getText();
-                }
-                else{
-                    licz = oText+"√(";
-                    oknoWynik.setText(licz);
-                }
-                przecinek = 0;
-
-
-
-            }
-        });
-        bPrzecinek.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
+            else {
                 if(przecinek == 0) {
 
                     if (oText.equals("0")) {
                         oknoWynik.setText("0,");
-                        licz = oknoWynik.getText();
                         przecinek = 1;
                     } else {
-                        oText = oText + ",";
-                        oknoWynik.setText(oText);
+                        oknoWynik.setText(oknoWynik.getText() + bPrzecinek.getText());
                         przecinek = 1;
                     }
-                }
-
-
-
 
             }
-        });
-        bWynik.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                String dane = String.valueOf(oknoWynik.getText());
-
-                StringBuffer str1 = new StringBuffer(dane);
-                String str2 = str1.toString().replaceAll(",", ".");
-
-                StringBuffer str3 = new StringBuffer(str2);
-                String str4 = str3.toString().replaceAll("poteguj", "Math.pow");
-
-                StringBuffer str5 = new StringBuffer(str4);
-                String str6 = str5.toString().replaceAll("√", "Math.sqrt");
-
-                StringBuffer str7 = new StringBuffer(str6);
-                String str8 = str7.toString().replaceAll("÷", "/");
-
-                StringBuffer str9 = new StringBuffer(str8);
-                dane = str9.toString().replaceAll("do", ",");
-
-
-
-
-                try {
-                    Wynik.Wynik(dane);
-                    oknoWynik.setText(Wynik.Wynik(dane));
-                    if (Wynik.Wynik(dane).equals("Infinity"))
-                        oknoWynik.setText("Nie dziel przez 0!");
-                } catch (Exception e) {
-                    oknoWynik.setText("Błąd składni!!");
-                    e.printStackTrace();
-                }
 
             }
-        });
-        bDo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oText = oknoWynik.getText();
-                iText = " do ";
-                if(oText.equals("0")) {
-                    oknoWynik.setText("0");
-                }
-                else
-                    oknoWynik.setText(oText + iText);
+            });
+        bWynik.addActionListener(evt -> {
+            String dane = String.valueOf(oknoWynik.getText());
+
+            StringBuffer str1 = new StringBuffer(dane);
+            String str2 = str1.toString().replaceAll(",", ".");
+
+            StringBuffer str3 = new StringBuffer(str2);
+            String str4 = str3.toString().replaceAll("poteguj", "Math.pow");
+
+            StringBuffer str5 = new StringBuffer(str4);
+            String str6 = str5.toString().replaceAll("√", "Math.sqrt");
+
+            StringBuffer str7 = new StringBuffer(str6);
+            String str8 = str7.toString().replaceAll("÷", "/");
+
+            StringBuffer str9 = new StringBuffer(str8);
+            dane = str9.toString().replaceAll("do", ",");
+
+
+
+
+            try {
+                Wynik.Wynik(dane);
+                oknoWynik.setText(Wynik.Wynik(dane));
+                if (Wynik.Wynik(dane).equals("Infinity"))
+                    oknoWynik.setText("Nie dziel przez 0!");
+            } catch (Exception e) {
+                oknoWynik.setText("Błąd składni!!");
+                e.printStackTrace();
             }
+
         });
+        bDo.addActionListener(evt -> buttonDzialanieAction(bDo));
+
+    }
+
+
     }
 
 
 
-}
 
 
