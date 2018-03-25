9e4a7500f9d45caff4188fb0b9ec688c9a46cda6
DESKTOP-P5A2NA1\bunze <bunzeladam@hotmail.com>
2018-03-25 23:59:02 +0200

    Refactory, newy class

--- src/Kalkulator/Kalkulator.java
+++ src/Kalkulator/Kalkulator.java
@@ -1,7 +1,10 @@
+
 public class Kalkulator {
     public static void main(String[] args) {
-        MyFrame okno = new MyFrame();
+        buttonActionListener okno = new buttonActionListener();
 
 
     }
+
+
 }
